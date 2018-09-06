ac0d554861bbd457273ec39be2eea47fdf9db100
Jemadean Dobrosielski <jemadean@gmail.com>
2018-09-06 11:41:10 -0700

    created validators

---
+++ src/validators.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Create synchronous validators which check that the Tracking Number field:
+// Contains a value
+// The value is non-empty
+// The value is be exactly 5 characters long
+// Each character is a number
+
+export const required = value => value ? undefined : 'Required';
+export const nonEmpty = value => value.trim() !== ''? undefined : 'Cannot be empty';
+export const requiredLength = value => value.length === 5 ? undefined : 'Tracking number must be 5 characters long';
+export const eachCharacterNumber = value => value.split('').find(character => isNaN(character) || character === ' ') ? 'Must be all numbers' : undefined;
