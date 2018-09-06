867c787d33bc20b05b32932f48d59af7f44bf17a
Burkey Koontz <bk.koontz@gmail.com>
2018-09-06 12:21:38 -0700

    deleted duplicate labels

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,12 +7,10 @@ export function Form(props) {
   return(
     <form>
       <div>
-        <label htmlFor="trackingNumber">Tracking number</label>
-        <Field name="trackingNumber" id="trackingNumber" component={Input} validate={[required, nonEmpty, requiredLength, requiredAllNumbers]} />
+        <Field label="Tracking number" name="trackingNumber" id="trackingNumber" component={Input} validate={[required, nonEmpty, requiredLength, requiredAllNumbers]} />
       </div>
       <div>
-        <label htmlFor="issue">What is your issue?</label>
-        <Field  name="issue" id="issue" component={Input} element="select">
+        <Field label="What is your issue?" name="issue" id="issue" component={Input} element="select">
           <option value="not-delivered">My delivery hasn't arrived</option>
           <option value="wrong-item">The wrong item was delivered</option>
           <option value="missing-part">Part of my order was missing</option>
@@ -21,8 +19,7 @@ export function Form(props) {
         </Field>
       </div>
       <div>
-        <label htmlFor="details">Give more details (optional)</label>
-        <Field name="details" id="details" element="textarea" component={Input} />
+        <Field label="Give more details (optional)" name="details" id="details" element="textarea" component={Input} />
       </div>
       <div>
         <button type="submit">Submit</button>
