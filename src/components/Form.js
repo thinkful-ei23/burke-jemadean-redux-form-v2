2f5536176db02fb761acee0f10931d57ca68f676
Burkey Koontz <bk.koontz@gmail.com>
2018-09-06 10:56:35 -0700

    changed inputs to Field components

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,27 +2,25 @@ import React from 'react';
 import {reduxForm, Field} from 'redux-form';
 
 export function Form(props) {
-  // <Field name="name" id="name" type="text" component="input" />
   return(
     <form>
       <div>
-        <label for="trackingNumber">Tracking number</label>
+        <label htmlFor="trackingNumber">Tracking number</label>
         <Field name="trackingNumber" id="trackingNumber" component="input" />
-        {/* <input name="trackingNumber" id="trackingNumber"></input> */}
       </div>
       <div>
-        <label for="issue">What is your issue?</label>
-        <select name="issue" id="issue">
+        <label htmlFor="issue">What is your issue?</label>
+        <Field name="issue" id="issue" component="select">
           <option value="not-delivered">My delivery hasn't arrived</option>
           <option value="wrong-item">The wrong item was delivered</option>
           <option value="missing-part">Part of my order was missing</option>
           <option value="damaged">Some of my order arrived damaged</option>
           <option value="other">Other (give details below)</option>
-        </select>
+        </Field>
       </div>
       <div>
-      <label for="details">Give more details (optional)</label>
-        <textarea name="details" id="details"></textarea>
+        <label htmlFor="details">Give more details (optional)</label>
+        <Field name="details" id="details" component="textarea" />
       </div>
       <div>
         <button type="submit">Submit</button>
